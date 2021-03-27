28ec38183972664fe75f00b889a01641bfc542ab
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-27 14:11:13 -0500

    renamed test methods to be prefxed with test_ as per pytest convention

--- test_functions.py
+++ test_functions.py
@@ -21,12 +21,12 @@ class TestFunctions(unittest.TestCase):
     def __init__(self):
         print("TesterFunctions class init")
 
-    def bmiCalculationTest(self):
+    def test_bmiCalculation(self):
         assert math.isclose(Functions.getBMI(5,9,141),21.323, rel_tol = 0.001)
         assert math.isclose(Functions.getBMI(4,11,150),31.026, rel_tol = 0.001)
         assert math.isclose(Functions.getBMI(6,2,195),25.639, rel_tol = 0.001)
         
-    def bmiCategoryTest(self):
+    def test_bmiCategory(self):
         assert Functions.getBMICategory(16.0) == "underweight"
         assert Functions.getBMICategory(18.4) == "underweight"
         assert Functions.getBMICategory(18.5) == "normal"
@@ -39,7 +39,7 @@ class TestFunctions(unittest.TestCase):
         assert Functions.getBMICategory(30.1) == "obese"
         assert Functions.getBMICategory(50.0) == "obese"
 
-    def retirementAgeCalculationTest(self):
+    def test_retirementAgeCalculation(self):
         assert math.isclose(Functions.getBMI(25,65000,10,1500000),  96, rel_tol = 0)
         assert math.isclose(Functions.getBMI(45,100000,15,500000), 70, rel_tol = 0)
         assert math.isclose(Functions.getBMI(10,100000,12,700000), 54, rel_tol = 0)
@@ -48,7 +48,7 @@ class TestFunctions(unittest.TestCase):
         assert math.isclose(Functions.getBMI(80,100000,100,0), 80, rel_tol = 0)
         assert math.isclose(Functions.getBMI(100,50,57,1000000), 26091, rel_tol = 0)
 
-    def retirementAgeCategoryTest(self):
+    def test_retirementAgeCategory(self):
         assert Functions.getRetirementCategory(70.0) == "will meet"
         assert Functions.getRetirementCategory(99.0) == "will meet"
         assert Functions.getRetirementCategory(99.9) == "will meet"
