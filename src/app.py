5cb53d4c8fcc5a4d02be1f93f6ae58d27d914002
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-27 16:29:24 -0500

    sample webpage code

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
-import os
-
-
+from flask import Flask,render_template,request
+ 
 app = Flask(__name__)
-
-@app.route("/") 
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug = True, host = '0.0.0.0',port = int(os.environ.get('PORT',8080)))
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+app.run(host='localhost', port=5000)
