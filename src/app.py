74ebf0c034034b2f679166c7319821bef8b8342d
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-28 14:19:22 -0500

    Website almost fully functional

--- src/app.py
+++ src/app.py
@@ -1,18 +1,39 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, request
+import Functions
  
 app = Flask(__name__)
  
-@app.route('/form')
-def form():
-    return render_template('form.html')
+@app.route('/')
+def main():
+    return render_template('app.html')
+
+#submit bmi
+@app.route('/getbmi',methods=["POSTBMI"])
+def send():
+    if request.method == 'POSTBMI':
+        #pull data
+        feet = int(request.form['feet'])
+        inches = int(request.form['inches'])
+        pounds = float(request.form['pounds'])
+
+        if (feet * 12 + inches <= 0):
+            return render_template('app.html',bmi="Your total height must be greater than 0")
+        elif (pounds <= 0):
+            return render_template('app.html',bmi="Surely you weight something")
+        elif ((feet * 12 + inches > 0) and pounds > 0):
+            return render_template('app.html',bmi=("%.3f" % Functions.getBMI(feet,inches,pounds)) + " BMI")
+        else:
+             return render_template('app.html',bmi="")
+
+@app.route('/getretirementage',methods=["POSTRETAGE"])
+def sendRetAge():
+    if request.method == 'POSTRETAGE':
+        #pull data
+        age = float(request.form['age'])
+        anualSalary = float(request.form['anualSalary'])
+        percentSasved = float(request.form['percentSasved'])
+        desiredSavings = float(request.form['desiredSavings'])
+
+        return render_template('app.html',retirementAge="")
  
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
- 
- 
-app.run(host='localhost', port=5000)
+app.run(debug = True, host='localhost', port=5000)
