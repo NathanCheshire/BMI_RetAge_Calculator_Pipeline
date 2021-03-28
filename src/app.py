db6785fcd83f608ffc75103e00bd7e19ca5256e1
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-03-28 15:13:46 -0500

    Validation for BMI done

--- src/app.py
+++ src/app.py
@@ -11,30 +11,59 @@ def main():
 @app.route('/getbmi',methods=["POST"])
 def send():
     #pull data
-    feet = int(request.form['feet'])
-    inches = int(request.form['inches'])
-    pounds = float(request.form['pounds'])
+    feet = str(request.form['feet'])
+    inches = str(request.form['inches'])
+    pounds = str(request.form['pounds'])
 
-    ##what if they entered strings?
+    #what if they entered strings?
+    if (not feet.isdigit):
+        return render_template('app.html',bmi="Your feet value must be a number")
+    elif (not inches.isdigit):
+        return render_template('app.html',bmi="Your inches value must be a number")
+    elif (not pounds.isdigit):
+        return render_template('app.html',bmi="Your pounds value must be a number")
+    
+    #cast to data types
+    feet = int(feet)
+    inches = int(inches)
+    pounds = float(pounds)
 
+    #proper number validation
     if (feet * 12 + inches <= 0):
         return render_template('app.html',bmi="Your total height must be greater than 0")
     elif (pounds <= 0):
         return render_template('app.html',bmi="Surely you weight something")
     elif ((feet * 12 + inches > 0) and pounds > 0):
-        return render_template('app.html',bmi=("%.3f" % Functions.getBMI(feet,inches,pounds)) + " BMI")
+        return render_template('app.html',bmi=
+        ("%.3f" % Functions.getBMI(feet,inches,pounds)) + " BMI")
     else:
         return render_template('app.html',bmi="")
 
 @app.route('/getretirementage',methods=["POST"])
 def sendRetAge():
     #pull data
-    age = float(request.form['age'])
-    anualSalary = float(request.form['anualSalary'])
-    percentSasved = float(request.form['percentSasved'])
-    desiredSavings = float(request.form['desiredSavings'])
+    age = str(request.form['age'])
+    anualSalary = str(request.form['anualSalary'])
+    percentSasved = str(request.form['percentSasved'])
+    desiredSavings = str(request.form['desiredSavings'])
 
-    #validate input here
+    #what if they entered strings?
+    if (not age.isdigit):
+        return render_template('app.html',bmi="Your age value must be a number")
+    elif (not anualSalary.isdigit):
+        return render_template('app.html',bmi="Your salary value must be a number")
+    elif (not percentSasved.isdigit):
+        return render_template('app.html',bmi="Your percetn saved value must be a number")
+    elif (not desiredSavings.isdigit):
+        return render_template('app.html',bmi="Your desired savings value must be a number")
+
+    #cast to data types
+    age = float(age)
+    anualSalary = float(anualSalary)
+    percentSasved = float(percentSasved)
+    desiredSavings = float(desiredSavings)
+
+    #make sure things that need to be greater than 0 are and if so, return calculation and category
 
     return render_template('app.html',retirementAge="null still working")
  
